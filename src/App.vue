f81789d9b9053d0efb3715566311a2d5438db387
Zuurakan Saparbaieva <zuurakans@gmail.com>
2022-06-13 09:10:08 +0300

    upload all files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <MyComponent/>
+  <router-view/>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue'
+export default {
+  setup() {
+    
+  },
+}
+</script>
+<style lang="scss">
+
+</style>
+
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+ <h1>Counter: {{counter}}</h1>
+</template>
+
+<script>
+export default {
+  setup(){
+    return{
+      counter:0
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
